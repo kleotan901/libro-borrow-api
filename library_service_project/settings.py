266feee4f22e32096006f71c6f42a2b6f7343c07
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-30 21:18:17 +0200

    Installed drf

--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "books",
 ]
 
@@ -55,8 +56,7 @@ ROOT_URLCONF = "library_service_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
