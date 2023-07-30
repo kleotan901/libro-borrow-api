2e29d47a5dea494c7e40064304fabc5ef8488ecf
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-30 23:14:49 +0200

    Implemented serializer & views for all endpoints

--- library_service_project/urls.py
+++ library_service_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("books.urls", namespace="books")),
+    path("api/users/", include("users.urls", namespace="users")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from users.serializers import UserSerializer, UserListSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserListSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
