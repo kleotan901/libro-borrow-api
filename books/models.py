477560d75f47cd235d641337e3698ab7de56e521
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-31 15:38:06 +0200

    Validated book inventory is not 0

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -7,7 +8,13 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=20, choices=ENUM)
-    inventory = models.IntegerField()
+    inventory = models.IntegerField(
+        validators=[
+            MinValueValidator(
+                limit_value=1, message="Amount of books can not be 0 or negative number"
+            )
+        ]
+    )
     daily_fee = models.DecimalField(decimal_places=2, max_digits=1200)
 
     class Meta:
