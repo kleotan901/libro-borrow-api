0ad60b9785ef1d1ad54dd8d46c37b7985a8b6b10
Tetiana Kozakova <kleotan901@gmail.com>
2023-08-01 10:58:40 +0200

    Overridden create method to decrease inventory by 1 for book

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,7 +1,11 @@
-from rest_framework import mixins
+from sys import stdout
+
+from rest_framework import mixins, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
+from books.models import Book
 from borrowings.models import Borrowing
 from borrowings.serializers import (
     BorrowingSerializer,
@@ -32,3 +36,14 @@ class BorrowingViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user_id=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        serializer.save()
+        borrowed_book = serializer.data["book_id"]
+        book = Book.objects.get(id=borrowed_book)
+        book.inventory -= 1
+        book.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
