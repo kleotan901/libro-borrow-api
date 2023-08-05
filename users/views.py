f131758d74958d62e895b5a1f8266d9f439c5abe
Tetiana Kozakova <kleotan901@gmail.com>
2023-08-05 12:22:11 +0200

    Overriden create method to saving connect_token in bd

--- users/views.py
+++ users/views.py
@@ -1,13 +1,23 @@
+from secrets import token_urlsafe
+
+from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from telegram_notifications.models import Notification
 from users.serializers import UserSerializer, UserListSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = get_user_model().objects.get(id=response.data["id"])
+        connect_token = token_urlsafe(8)
+        Notification.objects.create(user=user, connect_token=connect_token)
+        return response
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
