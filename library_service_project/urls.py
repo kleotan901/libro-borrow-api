5835fe999e0f4a71332cd36f7fbee274a3142033
Tetiana Kozakova <kleotan901@gmail.com>
2023-08-05 11:42:23 +0200

    Created telegram_notifications app

--- library_service_project/urls.py
+++ library_service_project/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("books.urls", namespace="books")),
     path("api/users/", include("users.urls", namespace="users")),
     path("api/borrowings/", include("borrowings.urls", namespace="borrowings")),
+    path(
+        "api/telegram_notifications/",
+        include("telegram_notifications.urls", namespace="telegram-notifications"),
+    ),
 ]
---
+++ telegram_notifications/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from telegram_notifications.views import NotificationViewSet
+
+app_name = "telegram-notifications"
+
+router = routers.DefaultRouter()
+router.register("", NotificationViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
