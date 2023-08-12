392f3004079ae12b1699404b068261701579911c
Tetiana Kozakova <kleotan901@gmail.com>
2023-08-12 20:06:50 +0200

    Added description for /api/users/token/ endpoint to documentation

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from users.views import ManageUserView, CreateUserView
+from users.views import ManageUserView, CreateUserView, CustomTokenObtain
 
 app_name = "users"
 
 urlpatterns = [
     path("", CreateUserView.as_view(), name="create"),
-    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/", CustomTokenObtain.as_view(), name="token-obtain-pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
